42827e6d4fbf26cd72377fcca8fbf40540f78f32
arunakvinolan <steynarunkavi@gmail.com>
2025-07-08 09:46:57 -0700

    eror rectify in contact import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer"; 
-import Contact from "./components/contact/contact";
+// import Contact from "./components/contact/contact";
 import {
   BrowserRouter as Router,
   Route,
